790dc9746cd2b13940e1d90ac08c87f6259a2710
neta-r <neta2611@walla.com>
2023-12-25 14:56:42 +0200

    created and fixed reservation failed and succeed

--- PizzaApp/app/src/main/java/com/gambelingapp/Reservation/ReservationFailedFragment.java
+++ PizzaApp/app/src/main/java/com/gambelingapp/Reservation/ReservationFailedFragment.java
@@ -2,24 +2,28 @@ package com.gambelingapp.Reservation;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
 import com.gambelingapp.R;
 import com.gambelingapp.ReservationObject;
 
-public class ReservationSuccessFragment extends Fragment{
+
+public class ReservationFailedFragment extends Fragment {
     Button Menu;
     EditText date, time;
     ReservationObject reservationObject;
 
-    public ReservationSuccessFragment() {
-        super(R.layout.fragment_reservation_seucsess);
+    public ReservationFailedFragment() {
+        super(R.layout.fragment_reservation_falied);
     }
     @SuppressLint("SetTextI18n")
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -27,11 +31,9 @@ public class ReservationSuccessFragment extends Fragment{
         reservationObject= (ReservationObject) bundle.getParcelable("ResObj");
         date = (EditText) view.findViewById(R.id.Date);
         time = (EditText) view.findViewById(R.id.Time);
-        date.setText("We will see you on "+reservationObject.getDate());
+        date.setText("We are fully booked on "+reservationObject.getDate());
         time.setText("at "+reservationObject.getTime());
         Menu = (Button) view.findViewById(R.id.Menu);
 
     }
-
 }
-
