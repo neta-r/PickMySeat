bf2b2fd61662d46a182990adc708b5960a6c1a2e
neta-r <neta2611@walla.com>
2023-12-28 16:21:48 +0200

    added pizza images to recycle view

--- PizzaApp/app/src/main/java/com/gambelingapp/menu/MenuActivity.java
+++ PizzaApp/app/src/main/java/com/gambelingapp/menu/MenuActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,11 +15,15 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.gambelingapp.R;
 
 
+import org.checkerframework.checker.units.qual.A;
+
 import java.util.ArrayList;
 
 public class MenuActivity extends AppCompatActivity {
     RecyclerView rv;
     ArrayList<String> pizzaNames;
+
+    ArrayList<Integer> pizzaImages;
     LinearLayoutManager linearLayoutManager;
     pizzaAdapter adapter;
 
@@ -27,8 +32,9 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
         rv = findViewById(R.id.pizzaMenu);
         setPizzaNames();
+        setPizzaImages();
         linearLayoutManager = new LinearLayoutManager(MenuActivity.this, LinearLayoutManager.HORIZONTAL, false);
-        adapter = new pizzaAdapter(pizzaNames);
+        adapter = new pizzaAdapter(pizzaNames,pizzaImages);
         rv.setLayoutManager(linearLayoutManager);
         rv.setAdapter(adapter);
     }
@@ -43,10 +49,22 @@ public class MenuActivity extends AppCompatActivity {
         pizzaNames.add("Plain");
     }
 
+    void setPizzaImages(){
+        pizzaImages = new ArrayList<>();
+        pizzaImages.add(R.drawable.pizza1);
+        pizzaImages.add(R.drawable.pizza2);
+        pizzaImages.add(R.drawable.pizza3);
+        pizzaImages.add(R.drawable.pizza4);
+        pizzaImages.add(R.drawable.pizza5);
+        pizzaImages.add(R.drawable.pizza6);
+    }
+
     class pizzaAdapter extends RecyclerView.Adapter<pizzaAdapter.pizzaHolder>{
         ArrayList<String> pizzaNames;
-        public pizzaAdapter(ArrayList<String> pizzaNames) {
+        ArrayList<Integer> pizzaImages;
+        public pizzaAdapter(ArrayList<String> pizzaNames, ArrayList<Integer> pizzaImages) {
             this.pizzaNames=pizzaNames;
+            this.pizzaImages=pizzaImages;
         }
 
         @NonNull
@@ -60,6 +78,7 @@ public class MenuActivity extends AppCompatActivity {
         @Override
         public void onBindViewHolder(@NonNull pizzaHolder holder, int position) {
             holder.pizzaName.setText(pizzaNames.get(position));
+            holder.pizzaImage.setImageResource(pizzaImages.get(position));
         }
 
         @Override
@@ -69,10 +88,12 @@ public class MenuActivity extends AppCompatActivity {
 
         class pizzaHolder extends RecyclerView.ViewHolder{
             TextView pizzaName;
+            ImageView pizzaImage;
 
             public pizzaHolder(@NonNull View itemView) {
                 super(itemView);
                 this.pizzaName =itemView.findViewById(R.id.pizzaName);
+                this.pizzaImage = itemView.findViewById(R.id.pizzaImage);
             }
         }
     }
